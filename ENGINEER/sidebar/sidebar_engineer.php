d000fa608c7f789d136d0c19cc700812101540a9
jrreyes-jpg <jrreyes@ccc.edu.ph>
2026-09-01 21:01:29 +0800

    feat: Simplify project view by removing archive functionality and updating navigation

--- ENGINEER/sidebar/sidebar_engineer.php
+++ ENGINEER/sidebar/sidebar_engineer.php
@@ -1,11 +1,9 @@
 <?php
 $currentPath = str_replace('\\', '/', parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH) ?? '');
-$currentQuery = parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_QUERY) ?? '';
 $currentFile = basename($currentPath);
 
 $isOverview = in_array($currentFile, ['overview.php', 'engineer_dashboard.php'], true);
-$isArchive = $currentFile === 'projects.php' && (str_contains($currentQuery, 'view=trash') || str_contains($currentQuery, 'view=archive'));
-$isProjects = $currentFile === 'projects.php' && !$isArchive;
+$isProjects = $currentFile === 'projects.php';
 $isProcurement = $currentFile === 'procurement.php';
 $isInspections = $currentFile === 'site_inspections.php';
 $isQuotations = in_array($currentFile, ['quotations.php', 'quotation_form.php'], true);
@@ -102,23 +100,6 @@ $isUpdates = $currentFile === 'progress_updates.php';
                     <span class="menu-mini-label">Proj</span>
                 </span>
                 <span class="menu-text">My Projects</span>
-            </a>
-        </li>
-        <li>
-            <a href="/codesamplecaps/ENGINEER/dashboards/projects.php?view=trash" class="menu-link<?php echo $isArchive ? ' active-link' : ''; ?>">
-                <span class="menu-visual" aria-hidden="true">
-                    <span class="menu-icon">
-                        <svg class="menu-icon-svg" viewBox="0 0 24 24" focusable="false" aria-hidden="true">
-                            <path d="M5 7h14"></path>
-                            <path d="M9 7V5.5A1.5 1.5 0 0 1 10.5 4h3A1.5 1.5 0 0 1 15 5.5V7"></path>
-                            <path d="M7 7l.8 11a2 2 0 0 0 2 1.85h4.4a2 2 0 0 0 2-1.85L17 7"></path>
-                            <path d="M10 11v5"></path>
-                            <path d="M14 11v5"></path>
-                        </svg>
-                    </span>
-                    <span class="menu-mini-label">Arch</span>
-                </span>
-                <span class="menu-text">Archive</span>
             </a>
         </li>
         <li>
